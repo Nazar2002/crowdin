5b4df905028dd6f69b8c8e6b9ba37c007c78cde7
nazar2002 <nazarhavuchak@gmail.com>
2023-01-23 16:14:20 +0200

    first commit

---
+++ src/i18n.js
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(initReactI18next)
+    .use(Backend)
+    .init({
+        backend: { loadPath: "/translations/{{lng}}.json" },
+        lng: "en",
+        fallbackLng: "en",
+        interpolation: { escapeValue: false },
+    });
+
+export default i18n;
